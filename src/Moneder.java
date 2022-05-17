015c45844915921c205bd5dcc22d9e2f3efdc4e1
Oriol Torrent <orioltorrent@inspladelestany.cat>
2022-05-17 17:41:53 +0200

    Segon Commit, principi classe Moneder, principi classe moneda.

--- src/Moneder.java
+++ src/Moneder.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 
 public class Moneder {
+    //Atributs
+    ArrayList<Monedes> moneder;
+    float monaderActual;
+
+    //Constructor
+    public Moneder(ArrayList<Monedes> moneder, float monaderActual){
+        this.moneder = moneder;
+        this.monaderActual = monaderActual;
+    }
+
+    //Metodes
+    public void afegirMonedes(Monedes monedes){
+        moneder.add(monedes);
+    }
+
+    public void afegirQuantitatMonedes(String tipus, int quantiat){
+        for (Monedes a : moneder){
+            if (a.getTipus().equals(tipus)){
+                a.afegirMonedes(quantiat);
+                break;
+            }
+        }
+    }
 
 }
--- src/Monedes.java
+++ src/Monedes.java
@@ -1,4 +1,43 @@
 
 public class Monedes {
+    //Atributs
+    private String tipus;
+    private double valor;
+    private int quantiat;
 
+    //Constructor
+    public Monedes(String tipus, double valor, int quantiat){
+        this.tipus = tipus;
+        this.valor = valor;
+        this.quantiat = quantiat;
+    }
+
+    //Metodes
+    public String getTipus() {
+        return tipus;
+    }
+
+    public void setTipus(String tipus) {
+        this.tipus = tipus;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantiat() {
+        return quantiat;
+    }
+
+    public void setQuantiat(int quantiat) {
+        this.quantiat = quantiat;
+    }
+
+    public void afegirMonedes(int quantiat){
+        this.quantiat = this.quantiat + quantiat;
+    }
 }
