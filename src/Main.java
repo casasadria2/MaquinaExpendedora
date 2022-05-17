05ec4026950cb1dfbbce6f45ecc8eb9914455807
jhoelam <jhoelalvarez@inspladelestany.cat>
2022-05-17 17:31:47 +0200

    asd

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("juan");
         System.out.println("prova");
+        System.out.prinln("prova2");
     }
 
  
